c7dcf0d6cd20869ab8d889ccee60d4924ff1dc2d
gahuza <gahuzaj12@gmail.com>
2025-01-21 09:03:03 -0800

    well

---
+++ jado-vite/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex  w-ful' >
+        <input type='text' className='bg-slate-200 outline-none w-full'/>
+        
+
+    </div>
+  )
+}
+
+export default Header
