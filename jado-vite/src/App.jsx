b7e98fcc795af5b6d2748a51cff73629e998c755
gahuza <gahuzaj12@gmail.com>
2025-01-23 07:23:41 -0800

    count

--- jado-vite/src/App.jsx
+++ jado-vite/src/App.jsx
@@ -4,40 +4,80 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-   
-    <Home/>
-    <Header/>
-      {/* <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ // const [count, setCount] = useState(0)
+
+ class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count:0,
+    }
+
+    // this.state = {
+    //   user: null,
+    // };
+  }
+  incrementCount=()=>{
+    this.setState((prevstate)=>({
+    count:prevstate.count +1
+    }))
+  }
+
+  decrementcount=()=>{
+    this.setState((laststate)=>({
+      count: laststate.count -1
+    }))
+  }
+
+  updateCount=()=>{
+    this.setState((upState)=({
+      count:upState.count * 20
+    }))
+  }
+    componentDidMount() {
+    // Fetching data from an API when the component is mounted
+
+    
+    // fetch('https://jsonplaceholder.typicode.com/users/1')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ user: data }));
+  }
+
+  render(){
+    return (
+      <div className='App'>
+     
+      <Home/>
+      <Header/>
+      <h1>Count:{this.state.count}</h1>
+      <button onClick={this.incrementCount} className='bg-{color}-gray'>Increment</button> <br/> <br/>
+      <button onClick={this.decrementcount}>Decrement</button> <br/> <br/>
+      <button onClick={this.updateCount}>Update State</button>
+
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Welcome to our react project
-      </p> */}
-    </>
-  )
+    )
+
+    // const { user } = this.state;
+    // return (
+    //   <div>
+    //     {user ? (
+    //       <div>
+    //         <h1>{user.name}</h1>
+    //         <p>{user.email}</p>
+    //       </div>
+    //     ) : (
+    //       <p>Loading...</p>
+    //     )}
+    //   </div>
+    // );
+  
+
+  }
+
 }
 
 export default App
--- jado-vite/src/components/Header.jsx
+++ jado-vite/src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='flex  w-ful' >
+      
         <input type='text' className='bg-slate-200 outline-none w-full'/>
         
 
